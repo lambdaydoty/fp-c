96c3bdac475a041120532534ca71e13cf50dfbdc
lambdaydoty <euphrates.tigris@gmail.com>
2020-07-31 12:43:21 +0000

    Add 2.1.2

---
+++ 2.1.2_A_computational_model_for_C_programs.c
@@ -0,0 +1,23 @@
+/**
+ * A C Program = A list of function declarations: [main, ...]
+ * A C Function = A list of statements.
+ * A statement is an operation on the `state` of the program.
+ */
+
+/**
+ * 1) The `return`-statement terminates the executation of a function and returns a result value.
+ *
+ * 2) The expression-statement evaluates an expression, ignoring the result.
+ *
+ * 3) The if-statement conditionally executes other statements.
+ *
+ */
+
+/* A simple C program, it prints `Hello world` */
+
+#include <stdio.h>
+
+int main (void) {
+  printf("Hello world\n");
+  return 0;
+}
