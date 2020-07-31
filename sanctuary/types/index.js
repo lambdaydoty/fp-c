d3ac4833f7ee08028990118f9e25cf71bcccb1ad
lambdaydoty <euphrates.tigris@gmail.com>
2020-07-31 01:14:53 +0000

    Remove js dependency

--- sanctuary/types/index.js
+++
@@ -1,8 +0,0 @@
-/* eslint func-call-spacing: ["error", "always"] */
-/* eslint no-multi-spaces: ["error", { ignoreEOLComments: true }] */
-module.exports = {
-  ...require ('./Unit'),
-  ...require ('./List'),
-  ...require ('./Reader'),
-  // ...require ('./Writer'),
-}
