422c72473bbb65c442e6eac358efe96fcbf5a1d3
lambdaydoty <euphrates.tigris@gmail.com>
2020-10-02 06:07:47 +0000

    Update 161

--- cracking/js/161-one-edit-distance.js
+++ cracking/js/161-one-edit-distance.js
@@ -1,43 +1,61 @@
 /**
- * @param {string} s
- * @param {string} t
+ * @param {string} S
+ * @param {string} T
  * @return {boolean}
  */
-module.exports = function isOneEditDistance (s_, t_) {
-  const [s, t] = reordering(s_, t_)
-  const lenS = s.length
-  const lenT = t.length
-  let lenD = lenS - lenT
-  /* lenS >= lenT */
+module.exports = function isOneEditDistance (S_, T_) {
+  const [S, T] = reordering(S_, T_)
+  const [lenS, lenT] = [S.length, T.length]
+  const lenD = lenS - lenT
+
+  /* Assume: lenS >= lenT, lenD = 0, 1, 2, ... */
+
   if (lenD >= 2) return false
 
-  let i = 0; let j = 0
-  while (s[i] === t[j] && j < lenT) { i++; j++ }
+  /**
+    * Start to consume the same prefix of S and T
+    * Maintain two index-pointers s, t
+    */
 
-  /*
-   * abcd   xbcd   abcd
-   * xbcd   abc    bcd
+  let [s, t] = [0, 0]
+  ;[s, t] = consumeSameChars(s, t)
+
+  /**
+   * The ending condition for `consumeSameChars()` are either
+   * A) s === lenS (the end of S string) or
+   * B) t === lenT (the end of T string) or
+   * C) S[s] !== T[t] for some middle posistions of strings S, T
    */
-  for (; i < lenS; i++) {
-    if (s[i] !== t[j] && lenD === 0) {
-      j++
-      lenD = -1
-      continue
-    }
-    if (s[i] !== t[j] && lenD === 1) {
-      lenD = -1
-      continue
-    }
-    if (s[i] !== t[j] && lenD === -1) {
-      return false
-    }
-    j++
+
+  if (s === lenS && t === lenT) return false // same strings
+
+  /**
+   * Examples:
+   *   lenD | 0      1      1
+   *   S    | abcd   xbcd   abcd
+   *   T    | xbcd   abc    bcd
+   */
+
+  if (lenD === 0) {
+    /* We are allowed to replace one by another at position [s, t]; thus skip both  */
+    ;[s, t] = consumeSameChars(s + 1, t + 1)
   }
-  return (lenD === -1)
+  if (lenD === 1) {
+    /* We are allowed to delete S[s]; thus skip it  */
+    ;[s, t] = consumeSameChars(s + 1, t)
+  }
+
+  return (s === lenS && t === lenT)
 
   function reordering (a, b) {
-    return (a.length >= b.length)
-      ? [a, b]
-      : [b, a]
+    return (a.length >= b.length) ? [a, b] : [b, a]
+  }
+
+  /**
+   * Consume the same characters from [s, t] and return the new indexed
+   */
+  function consumeSameChars (s, t) {
+    while (s < lenS && t < lenT && S[s] === T[t]) { s++; t++ }
+    return [s, t]
   }
 }
--- cracking/js/test/161-one-edit-distance.test.js
+++ cracking/js/test/161-one-edit-distance.test.js
@@ -3,9 +3,12 @@ const fn = require('../161-one-edit-distance')
 test('Examples', () => {
   ;[
     ['ab', 'acb'],
+    ['abcd', 'bcd'],
   ].forEach(([s, t]) => expect(fn(s, t)).toBe(true))
 
   ;[
+    ['xbcd', 'abc'],
     ['', ''],
+    ['acbbcda', 'abbdad'],
   ].forEach(([s, t]) => expect(fn(s, t)).toBe(false))
 })
